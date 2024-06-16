e4f1cf202a5f4bba532b0ba33511653de4d27cb5
simalina554 <98891098+simalina554@users.noreply.github.com>
2024-06-16 11:12:38 +0300

    Update 2ver.py

--- 2ver.py
+++ 2ver.py
@@ -1,6 +1,7 @@
 import cv2
 from PIL import Image
 import os
+import random
 
 font = cv2.FONT_ITALIC
 text = 'Esenin'
@@ -9,14 +10,25 @@ text_color = (32, 178, 170)
 border_thickness = 2
 image_dir = "image"
 
+
 for i in range(1, 5):
+    def random_word():
+        with open("question.txt", 'r', encoding="utf-8") as file:
+            lines = file.readlines()
+        random_lines = random.sample(lines, 1)
+        result_string = '|'.join(map(lambda x: x.strip(), random_lines))
+
+        return result_string
+
+
+    result1 = random_word()
     img = cv2.imread(f"image/image{i}.jpeg")
     img_h, img_w = img.shape[:2]
-    text_size = cv2.getTextSize(text, font, 1, border_thickness)[0]
+    text_size = cv2.getTextSize(result1, font, 1, border_thickness)[0]
     text_baseline = img_h - bottom_margin
     text_start_x = (img_w - text_size[0]) // 2
     text_pos = (text_start_x, text_baseline)
-    cv2.putText(img, text, text_pos, font, 1, text_color, border_thickness)
+    cv2.putText(img, result1, text_pos, font, 1, text_color, border_thickness)
     cv2.imshow('Result', img)
     cv2.imwrite(f"image/image{i}{i + 1}.jpeg", img)
     cv2.waitKey(0)
@@ -39,6 +51,11 @@ def create_gif(frames, gif_filename, duration=100, loop=0):
     )
 
 
+if os.path.exists('result'):
+    print('ok')
+else:
+    os.mkdir('result')
+
 image_filenames = os.listdir(image_dir)
 
 image_pairs = []
@@ -56,7 +73,7 @@ for i, (image1_filename, image2_filename) in enumerate(image_pairs):
         img_with_text = add_text_to_image(image_path)
         frames.append(img_with_text)
 
-    gif_filename = f"esenin_slideshow_{i + 1}.gif"
+    gif_filename = f"result/esenin_slideshow_{i + 1}.gif"
     create_gif(frames, gif_filename)
 
 print("Slideshow creation for all image pairs complete!")
